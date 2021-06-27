5e32ea10b07621168561594118db7594c6f29d6b
dmngtj <btcgiaivnsvntopencup@gmail.com>
2021-06-27 23:00:46 +0700

    sua

--- system/config.php
+++ system/config.php
@@ -3,7 +3,7 @@ define('HOST_DB','us-cdbr-east-04.cleardb.com');
 define('NAME_DB','heroku_e51a0bdfd5b9a88');
 define('USER_DB','b0aee9f6f88e08');
 define('PASS_DB','432966b5');
-define('ROOT_URL','/coffee');
+define('ROOT_URL','');
 define('ADMIN_URL',ROOT_URL.'/admin');
 define('SITE_URL',ROOT_URL.'/site/');
 define('SYSTEM_PATH',ROOT_URL.'/system');
